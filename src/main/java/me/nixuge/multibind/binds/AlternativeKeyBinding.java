96b9819e956ff1698c64fceaccb79b12a2c9cd70
Nixuge <mail@nixuge.me>
2023-06-27 15:55:55 +0200

    AlternativeKeyBinding & MixinKeyBinding

---
+++ src/main/java/me/nixuge/multibind/binds/AlternativeKeyBinding.java
@@ -0,0 +1,112 @@
+package me.nixuge.multibind.binds;
+
+import java.util.List;
+
+import com.google.common.collect.Lists;
+
+import net.minecraft.client.settings.KeyBinding;
+import net.minecraft.util.IntHashMap;
+
+@SuppressWarnings({"unchecked", "rawtypes"})
+public class AlternativeKeyBinding {
+    private static final List<AlternativeKeyBinding> alternativeKeybindArray = Lists
+            .<AlternativeKeyBinding>newArrayList();
+    private static final IntHashMap<AlternativeKeyBinding> alternativeHash = new IntHashMap();
+    private int keyCode;
+    private boolean pressed;
+    private int pressTime;
+    private KeyBinding parentKeybind;
+
+    public AlternativeKeyBinding(KeyBinding parent, int keyCode) {
+        this.parentKeybind = parent;
+        this.keyCode = keyCode;
+        alternativeKeybindArray.add(this);
+        alternativeHash.addKey(keyCode, this);
+    }
+
+    // Done
+    public static void onTick(int keyCode) {
+        if (keyCode == 0)
+            return;
+
+        AlternativeKeyBinding keybinding = (AlternativeKeyBinding) alternativeHash.lookup(keyCode);
+
+        if (keybinding != null)
+            ++keybinding.pressTime;
+    }
+
+    // Todo
+    public static void setKeyBindState(int keyCode, boolean pressed) {
+        if (keyCode == 0)
+            return;
+
+        AlternativeKeyBinding keybinding = (AlternativeKeyBinding) alternativeHash.lookup(keyCode);
+
+        if (keybinding != null)
+            keybinding.pressed = pressed;
+    }
+
+    // Done
+    public static void unPressAllKeys() {
+        for (AlternativeKeyBinding keybinding : alternativeKeybindArray) {
+            keybinding.unpressKey();
+        }
+    }
+
+    // Done
+    public static void resetKeyBindingArrayAndHash() {
+        alternativeHash.clearMap();
+
+        for (AlternativeKeyBinding keybinding : alternativeKeybindArray) {
+            alternativeHash.addKey(keybinding.keyCode, keybinding);
+        }
+    }
+
+    // Done
+    public boolean isKeyDown() {
+        return this.pressed;
+    }
+
+    // Done
+    public boolean isPressed() {
+        if (this.pressTime == 0) {
+            return false;
+        } else {
+            --this.pressTime;
+            return true;
+        }
+    }
+
+    // Done
+    private void unpressKey() {
+        this.pressTime = 0;
+        this.pressed = false;
+    }
+
+    // Done
+    public int getKeyCode() {
+        return this.keyCode;
+    }
+
+    // Done
+    public KeyBinding getParentKeybind() {
+        return this.parentKeybind;
+    }
+
+    // Done
+    public void setKeyCode(int keyCode) {
+        this.keyCode = keyCode;
+    }
+
+    // public int compareTo(KeyBinding p_compareTo_1_) {
+    //     int i = I18n.format(this.keyCategory, new Object[0])
+    //             .compareTo(I18n.format(p_compareTo_1_.keyCategory, new Object[0]));
+
+    //     if (i == 0) {
+    //         i = I18n.format(this.keyDescription, new Object[0])
+    //                 .compareTo(I18n.format(p_compareTo_1_.keyDescription, new Object[0]));
+    //     }
+
+    //     return i;
+    // }
+}
--- src/main/java/me/nixuge/multibind/mixins/client/settings/MixinKeyBinding.java
+++ src/main/java/me/nixuge/multibind/mixins/client/settings/MixinKeyBinding.java
@@ -1,10 +1,66 @@
 package me.nixuge.multibind.mixins.client.settings;
 
-import org.spongepowered.asm.mixin.Mixin;
+import java.util.ArrayList;
+import java.util.List;
 
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import me.nixuge.multibind.binds.AlternativeKeyBinding;
 import net.minecraft.client.settings.KeyBinding;
 
 @Mixin(KeyBinding.class)
 public class MixinKeyBinding {
-    
+    @Shadow
+    private boolean pressed;
+
+    public List<AlternativeKeyBinding> alternativeKeybinds;
+
+    public void addAlternativeBind(int keyCode) {
+        this.alternativeKeybinds.add(
+            new AlternativeKeyBinding((KeyBinding)(Object)this, keyCode)
+        );
+    }
+
+    @Inject(method = "<init>", at = @At("RETURN"))
+    public void KeyBinding(String description, int keyCode, String category, CallbackInfo ci) {
+        this.alternativeKeybinds = new ArrayList<>();
+    }
+
+    @Inject(method = "setKeyBindState", at = @At("RETURN"))
+    public void setKeyBindState(int keyCode, boolean pressed, CallbackInfo ci) {
+        AlternativeKeyBinding.setKeyBindState(keyCode, pressed);
+    }
+
+    @Inject(method = "onTick", at = @At("RETURN"))
+    public static void onTick(int keyCode, CallbackInfo ci) {
+        AlternativeKeyBinding.onTick(keyCode);
+    }
+
+    @Inject(method = "unPressAllKeys", at = @At("RETURN"))
+    public static void unPressAllKeys(CallbackInfo ci) {
+        AlternativeKeyBinding.unPressAllKeys();
+    }
+
+    @Inject(method = "resetKeyBindingArrayAndHash", at = @At("RETURN"))
+    public static void resetKeyBindingArrayAndHash(CallbackInfo ci) {
+        AlternativeKeyBinding.resetKeyBindingArrayAndHash();
+    }
+
+    @Inject(method = "isKeyDown", at = @At("RETURN"))
+    public void getKeyCode(CallbackInfoReturnable<Boolean> cir) {
+        boolean otherKeybindPressed = alternativeKeybinds.stream().anyMatch(keybind -> keybind.isKeyDown());
+        cir.setReturnValue(this.pressed || otherKeybindPressed);
+    }
+
+
+    @Inject(method = "getKeyCode", at = @At("RETURN"))
+    public void getKeyCode(CallbackInfo ci) {
+        System.out.println("getKeyCode called. This is a problem since we possible have more than 1 keycodes");
+        System.out.println("Todo: check how this is handled & if it's problematic");
+    }
 }
